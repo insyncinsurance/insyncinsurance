9e59642768fc560e7edd9be2d7f4c0fa62819a3a
Lewis Munday <lewis.munday@insyncinsurance.co.uk>
2022-09-22 11:13:22 +0100

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
->
+<!-- Placeholder Objects
+&#9744; Unchecked box
+&#9745; Checked box
+ -->
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://www.insyncinsurance.co.uk/">
+    <img src="./img/insync-logo.png" alt="Insync Insurance Logo" width="200" height="80">
+  </a>
+
+  <h3 align="center">Insync Insurance - ICE Rating</h3>
+
+  <p align="center">
+    Module Details and information
+    <br />
+    <a href="https://github.com/insyncinsurance/insyncinsurance"><strong>Explore the project »</strong></a>
+    <br />
+    <br />
+  </p>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    <ul>
+        <li><a href="#contributing">Contributing</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#intallation">Installation</a></li>
+        <li><a href="#documentation">Documentation</a></li>
+      </ul>
+    </li>
+    <li><a href="#contributors">Contributors</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+## About the Project
+
+The Insync Insurance module provides an easier, quicker and more efficient way to calculate within ICE rating for your insurance policy.
+
+### Built With
+
+* [Python](https://www.python.org) - ver: 2.7.10
+
+### Contributing
+If you'd like to contribute to this project, please see the [contributing guide](CONTRIBUTING.md).
+
+## Getting Started
+
+### Installation
+
+```bash
+pip install insyncinsurance
+```
+
+### Documentation
+The full documentation is available [here](https://www.insyncinsurance.co.uk/docs/).
+
+## Roadmap
+
+**Roadmap completion:** 0%
+
+&#9744; TBC
+
+&#9745; TBC
+
+## Contributors
+- [@Lewis Munday](https://www.github.com/lewismunday)
+
+## Contact
+
+For any queries regarding the module, please contact either of the below parties. 
+
+* [Kathryn Orledge](mailto:kathryn.orledge@insyncinsurance.co.uk), Digital Products Manager 
+* [Lewis Munday](mailto:lewis.munday@insyncinsurance.co.uk), Module Developer. 
--- src/insync/insyncinsurance.py
+++ src/insync/insyncinsurance.py
@@ -1,5 +1,11 @@
 import re
 
+def lt(a, b):
+    return a < b
+
+def le(a, b):
+    return a <= b
+
 refer = lambda code, *message: info('Refer', [code, ' '.join(map(str, message))])
 decline = lambda code, *message: info('Decline', [code, ' '.join(map(str, message))])
 
@@ -7,34 +13,35 @@ decline = lambda code, *message: info('Decline', [code, ' '.join(map(str, messag
 def create_message(code):
     def returned_function(*message):
         info('Refer', [code, ' '.join(map(str, message))])
+
     return returned_function
+
 
 debug = create_message('Debug')
 
 
-def getList(Level, Field):
-    tempList = []
+def get_list(level, field):
+    temp_list = []
     for part in parts:
-        if ge(part['partname'].find(Level), 0):
-            tempList.append(part[Field])
-    return tempList
+        if ge(part['partname'].find(level), 0):
+            temp_list.append(part[field])
+    return temp_list
 
-    
 
 class Policy:
     policies = {}
-    
+
     def __init__(self, name):
         self.premium = 0
         self.endorsements = []
         self.excess = {}
         self.name = name
         Policy.policies[name] = self.premium
-    
+
     def add(self, number):
         self.premium += number
         Policy.policies[self.name] = self.premium
-    
+
     def rate(self, rate):
         self.premium = round(self.premium * rate, 2)
         Policy.policies[self.name] = self.premium
@@ -42,125 +49,130 @@ class Policy:
     def override(self, amount):
         self.premium = amount
         Policy.policies[self.name] = self.premium
-    
+
     def add_endorsement(self, endorsement):
         if endorsement not in self.endorsements:
             self.endorsements.append(endorsement)
         else:
             pass
-    
-    def removeEndorsement(self, endorsement):
+
+    def remove_endorsement(self, endorsement):
         self.endorsements.remove(endorsement)
+
+    def get_endorsements(self):
+        return self.endorsements
 
     def discount(self, amount):
         self.premium -= amount
         Policy.policies[self.name] = self.premium
 
-    def getPremium(self):
+    def get_premium(self):
         return self.premium
-    
-    def combine(list1, list2):
-        return set(list(list1 + list2))
-    
-        
-    def addExcess(self, type, amount):
-        if type in self.excess:
-            self.excess[type] += amount
+
+    @staticmethod
+    def combine(*lists):
+        return [i for sub in lists for i in sub]
+
+    def add_excess(self, excess_type, amount):
+        if excess_type in self.excess:
+            self.excess[excess_type] += amount
         else:
-            self.excess[type] = amount
-    
-    def removeExcess(self, type, amount=None):
-        if amount == None:
-            self.excess.pop(type)
-        elif type in self.excess:
-            self.excess[type] -= amount
+            self.excess[excess_type] = amount
+
+    def remove_excess(self, excess_type, amount=None):
+        if amount is None:
+            self.excess.pop(excess_type)
+        elif excess_type in self.excess:
+            self.excess[excess_type] -= amount
         else:
             pass
-    
-    def rateExcess(self, type, rate):
-        self.excess[type] = round(self.excess[type] * rate, 2)
-    
-    def discountExcess(self, type, amount):
-        self.excess[type] -= amount
 
-    def getExcess(self, type):
-        return self.excess[type]
-    
-    def totalExcess(self):
+    def rate_excess(self, excess_type, rate):
+        self.excess[excess_type] = round(self.excess[excess_type] * rate, 2)
+
+    def discount_excess(self, excess_type, amount):
+        self.excess[excess_type] -= amount
+
+    def get_excess(self, excess_type):
+        return self.excess[excess_type]
+
+    def total_excess(self):
         return sum(self.excess.values())
-    
+
+    @staticmethod
     def total():
         return sum(Policy.policies.values())
+
+    @staticmethod
+    def stringify_total():
+        return str('{0:,.2f}'.format(float(sum(Policy.policies.values()))))
 
 
 class debugging:
     def __init__(self):
         pass
-    
+
     debug_mode = False
-    
+
     @staticmethod
     def toggle():
         debugging.debug_mode = not debugging.debug_mode
-    
+
     @staticmethod
     def send(*message):
         if debugging.debug_mode:
             debug(*message)
-    
+
     @staticmethod
     def breakdown():
         if debugging.debug_mode:
             for k, v in Policy.policies.items():
-                debug(k, 'has a premium of' , v)
+                debug(k, 'has a premium of', v)
 
 
-
-class Postcodes():   
+class Postcodes():
     def __init__(self, Postcode):
         self.Postcode = Postcode
         self.matches = re.findall(r'^(([A-Z][A-Z]{0,1})([0-9][A-Z0-9]{0,1})) {0,}(([0-9])([A-Z]{2}))$', self.Postcode)
-        
+
     def area(self):
         return self.matches[0][1]
-    
+
     def district(self):
         return self.matches[0][0]
-    
+
     def sector(self):
         return self.matches[0][0] + self.matches[0][4]
-    
+
     def unit(self):
         return self.matches[0][0] + self.matches[0][3]
-    
+
     def contains(self, comparative):
-        pUNit = self.matches[0][0] + self.matches[0][3]
+        postcode_unit = self.matches[0][0] + self.matches[0][3]
         if isinstance(comparative, list):
             for i in comparative:
-                if i in pUNit:
+                if i in postcode_unit:
                     return True
-                    break
-                else:
-                    return False
+            return False
         else:
-            if comparative in pUNit:
+            if comparative in postcode_unit:
                 return True
             else:
                 return False
-    
+
     def validate(self):
         pattern = 'not matched'
         pc = self.replace(" ", "")
-        #e.g. W27XX
+        # e.g. W27XX
         if len(pc.replace(" ", "")) == 5:
             pattern = re.compile("^[a-zA-Z]{1}[0-9]{2}[a-zA-Z]{2}")
-        #e.g. TW27XX
+        # e.g. TW27XX
         elif len(pc.replace(" ", "")) == 6:
             pattern = re.compile("^[a-zA-Z]{2}[0-9]{2}[a-zA-Z]{2}")
-        #e.g. TW218FF
+        # e.g. TW218FF
         elif len(pc.replace(" ", "")) == 7:
             pattern = re.compile("^[a-zA-Z]{2}[0-9]{3}[a-zA-Z]{2}")
-        
+
         if pattern != 'not matched':
             if pattern.match(pc):
                 return True
@@ -168,12 +180,12 @@ class Postcodes():
                 return False
         else:
             return False
-        
+
     def __repr__(self):
         return self.Postcode
 
 
-def tryAll(array):
+def try_all(array):
     errors = 0
     count = 0
     error_list = []
@@ -181,7 +193,7 @@ def tryAll(array):
     while lt(count, len(array)):
         variable = array[count]
         try:
-            exec("%s = %s" % (variable + "Dft", variable))
+            exec ("%s = %s" % (variable + "Dft", variable))
         except NameError:
             errors += 1
             error_list.append(variable)
@@ -189,10 +201,9 @@ def tryAll(array):
     if debug_mode:
         debug('All variables have been tested with', errors, 'errors out of', len(array), 'variables.')
         debug('The following variables have errors:', error_list)
-    
+
     if gt(errors, 0) and debug_mode == False:
-        refer('Error', errors , 'errors occurred obtaining Workbench fields. Enable debug mode to see the error.')
-    
+        refer('Error', errors, 'errors occurred obtaining Workbench fields. Enable debug mode to see the error.')
 
 
 def createDocValueDict(array, docValueType, blank='Nil'):
@@ -207,7 +218,6 @@ def createDocValueDict(array, docValueType, blank='Nil'):
     docValueDict = dict(zip(variableList, array))
 
     return docValueDict
-
 
 
 def addCover(coverCodeList, coverDescList, coverPriceList, coverLimitList, coverCode, coverDesc, coverPrice,
